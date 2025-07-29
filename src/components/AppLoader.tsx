6284d134f32015b04c6714e8ae7ffb0ff69f9323
Meursault <gameaccc333@gmail.com>
2025-07-29 22:25:16 +0300

    return loader

--- src/components/AppLoader.tsx
+++ src/components/AppLoader.tsx
@@ -8,7 +8,6 @@ export const AppLoader = ({ children }: { children: React.ReactNode }) => {
       <div className="w-full h-screen flex items-center justify-center  text-white text-xl bg-[url(/LoaderBG.webp)] bg-center">
         <Spin fullscreen className={'scale-500 bottom-[10vh] '} />
         <div className={'text-[30px] w-[100%] text-center mt-5'}>The site hasn't loaded yet</div>
-        {children}
       </div>
     );
   }
